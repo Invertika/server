7356d62dcaae80f5dc4027f9b99cadf315666381
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2010-08-10 15:49:45 +0000

    Tippfehler im Webservice korrigiert
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1638 784bf6b6-0765-11df-962c-b5c252e50527

--- start-webservice.sh
+++ start-webservice.sh
@@ -3,6 +3,6 @@
 while true
 do
   python3 webservice.py 2> last_crash_webservice_log.txt
-  echo "Letzter Webservicecrasg am `date`" > last_crash_webservice.txt
+  echo "Letzter Webservicecrash am `date`" > last_crash_webservice.txt
   sleep 5
 done
