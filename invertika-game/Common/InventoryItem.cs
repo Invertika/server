1dc0b27827b7180f3174c26c9b52ef122fcfad21
seeseekey <seeseekey@googlemail.com>
2012-03-28 15:55:44 +0200

    Mit dem Einbau des Skriptings begonnen.

---
+++ invertika-game/Common/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace invertika_game.Common
+{
+	/**
+	 * Structure storing an item in the inventory.
+	 */
+	public class InventoryItem
+	{
+
+		uint itemId;
+		uint amount;
+	}
+}
