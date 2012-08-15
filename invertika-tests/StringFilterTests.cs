498a6a9aa861be058f3ef4b8af968d1e3db4313c
seeseekey <seeseekey@gmail.com>
2012-08-15 13:58:22 +0200

    Test Projekt hinzugefügt.

---
+++ invertika-tests/StringFilterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using invertika_account.Utilities;
+
+namespace invertika_tests
+{
+	[TestFixture]
+	public class StringFilterTests
+	{
+		//[Test]
+		//public void TestNewStringFiler()
+		//{
+		//    StringFilter sf=new StringFilter();
+		//    Assert.AreEqual
+		//}
+	}
+}
